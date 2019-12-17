26d3cb3e99407ae962a92f9af6f729b66e4a89ff
vinay <vinay.ingalahalli1989@gmail.com>
2019-12-17 21:23:24 +0530

    static non_static

---
+++ revature_java_training/myfirstjavaproject/src/mypack/Demo.java
@@ -0,0 +1,12 @@
+package mypack;
+
+public class Demo {
+
+	public static void hey() {
+		System.out.println("Hello from static method of Demo class");
+	}
+	
+	public void demoHello() {
+		System.out.println("Hello non static demo");
+	}
+}
