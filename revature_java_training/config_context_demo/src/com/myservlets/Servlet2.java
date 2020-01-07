3d3981c829e3ba9729d17187e16bdf4040de75dd
vinay <vinay.ingalahalli1989@gmail.com>
2020-01-08 02:21:06 +0530

    config context

---
+++ revature_java_training/config_context_demo/src/com/myservlets/Servlet2.java
@@ -0,0 +1,46 @@
+package com.myservlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Servlet2
+ */
+public class Servlet2 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Servlet2() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		out.print("<h2>Accessing config params of Servlet1 in Servlet2");
+		ServletConfig config=getServletConfig();
+		out.print("<h2>Query 1 : "+config.getInitParameter("query1")+"</h2>");
+		out.print("<h2>Query 2 : "+config.getInitParameter("query2")+"</h2>");
+		out.print("<h2>Query 3 : "+config.getInitParameter("query3")+"</h2>");
+		
+		out.print("<br/><br/><h2>Accessing context params in Servlet2</h2>");
+		ServletContext context=getServletContext();
+		out.print("<h2>URL : "+context.getInitParameter("url")+"</h2>");
+		out.print("<h2>Username : "+context.getInitParameter("username")+"</h2>");
+		out.print("<h2>Driver : "+context.getInitParameter("driver")+"</h2>");
+	}
+
+}
--- revature_java_training/hello_servlet/src/com/app/servlets/MyServlet.java
+++ revature_java_training/hello_servlet/src/com/app/servlets/MyServlet.java
@@ -2,6 +2,7 @@ package com.app.servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Arrays;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -47,6 +48,9 @@ public class MyServlet extends HttpServlet {
 		response.setContentType("text/html");
 		PrintWriter out=response.getWriter();
 		out.print("<h1>Welcome "+request.getParameter("fname")+"  to doPost() of MyServlet</h1>");
+		out.print("<h2>Gender : "+request.getParameter("gender")+"</h2>");
+		out.print("<h2>Country : "+request.getParameter("country")+"</h2>");
+		out.print("<h2>Hobbies : "+Arrays.toString(request.getParameterValues("hobby"))+"</h2>");
 	}
 
 }
