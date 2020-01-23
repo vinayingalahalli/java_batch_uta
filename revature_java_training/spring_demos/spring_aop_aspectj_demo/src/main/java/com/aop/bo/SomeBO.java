30b13b536dc0e767e4b3746de852607630c9ce66
vinay <vinay.ingalahalli1989@gmail.com>
2020-01-24 00:42:50 +0530

    spring aop

---
+++ revature_java_training/spring_demos/spring_aop_aspectj_demo/src/main/java/com/aop/bo/SomeBO.java
@@ -0,0 +1,12 @@
+package com.aop.bo;
+
+public class SomeBO {
+
+	public void validate() {
+		System.out.println("some validation from validate() method of SomeBO");
+	}
+	
+	public void validateAgain() {
+		System.out.println("some repeated validation from validateAgain() method of SomeBO");
+	}
+}
